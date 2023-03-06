6d8192312d041742fa9b24fcbbd56b4c8a728674
Gillon Paterson <gillonpaterson@MacBook-Pro.local>
2023-03-06 12:33:14 +0000

    removing useless stuff

--- tests/seleniumTests/practiceUITests.js
+++ tests/seleniumTests/practiceUITests.js
@@ -1,18 +1,11 @@
 const { assert } = require("chai");
 var fs = require("fs");
 const chrome = require("selenium-webdriver/chrome");
-const firefox = require("selenium-webdriver/firefox");
 const { Builder } = require("selenium-webdriver");
-
-const screen = {
-  width: 640,
-  height: 480
-};
 
 let driver = new Builder()
     .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
-    .setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+    .setChromeOptions(new chrome.Options().headless())
     .build();
 
 async function example(){
