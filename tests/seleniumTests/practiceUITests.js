d60c5b035d570f648c226ba5c1d9e35cdb06e609
Gillon Paterson <gillonpaterson@MacBook-Pro.local>
2023-03-06 12:54:52 +0000

    adding chrome stuff

--- tests/seleniumTests/practiceUITests.js
+++ tests/seleniumTests/practiceUITests.js
@@ -2,31 +2,29 @@ const { assert } = require("chai");
 var fs = require("fs");
 const chrome = require("selenium-webdriver/chrome");
 const { Builder } = require("selenium-webdriver");
-const TIMEOUT = 30000000;
+let TIMEOUT = 30000;
 
 let driver = new Builder()
     .forBrowser("chrome")
     .setChromeOptions(new chrome.Options().headless())
     .build();
 
+    async function example(){
 
+      await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
+        TIMEOUT, script: TIMEOUT } );
+        console.info( await driver.manage().getTimeouts() );
 
-async function example(){
-
-      //await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
-        //TIMEOUT, script: TIMEOUT } )
-        //console.info( await driver.manage().getTimeouts() )
-
-        //await driver.get("http://google.com");
+        await driver.get("http://google.com");
              
-        //var title = await driver.getTitle();
-        //assert.strictEqual(title, "Google");
+        var title = await driver.getTitle();
+        assert.strictEqual(title, "Google");
         
-        //var htmlSource = await driver.getPageSource();
-        //fs.appendFile("src/assets/snapshots/google-snapshot.html", htmlSource, function (err) {
-        //  if (err) throw err;
-        //  console.log("Saved!");
-        //});
+        var htmlSource = await driver.getPageSource();
+        fs.appendFile("src/assets/snapshots/google-snapshot.html", htmlSource, function (err) {
+          if (err) throw err;
+          console.log("Saved!");
+        });
  
         //It is always a safe practice to quit the browser after execution
         await driver.quit();
