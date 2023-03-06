583d935db9b2349f5aac87308cfda59b5017e1a6
Gillon Paterson <gillonpaterson@MacBook-Pro.local>
2023-03-06 12:52:18 +0000

    adding chrome stuff

--- tests/seleniumTests/practiceUITests.js
+++ tests/seleniumTests/practiceUITests.js
@@ -13,20 +13,20 @@ let driver = new Builder()
 
 async function example(){
 
-      await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
-        TIMEOUT, script: TIMEOUT } )
-        console.info( await driver.manage().getTimeouts() )
+      //await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
+        //TIMEOUT, script: TIMEOUT } )
+        //console.info( await driver.manage().getTimeouts() )
 
-        await driver.get("http://google.com");
+        //await driver.get("http://google.com");
              
-        var title = await driver.getTitle();
-        assert.strictEqual(title, "Google");
+        //var title = await driver.getTitle();
+        //assert.strictEqual(title, "Google");
         
-        var htmlSource = await driver.getPageSource();
-        fs.appendFile("src/assets/snapshots/google-snapshot.html", htmlSource, function (err) {
-          if (err) throw err;
-          console.log("Saved!");
-        });
+        //var htmlSource = await driver.getPageSource();
+        //fs.appendFile("src/assets/snapshots/google-snapshot.html", htmlSource, function (err) {
+        //  if (err) throw err;
+        //  console.log("Saved!");
+        //});
  
         //It is always a safe practice to quit the browser after execution
         await driver.quit();
