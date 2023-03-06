2d0b4716da846e6e68240dc36f1175e0519ee1c6
Declan Larkin <declanlarkin@MacBook-Pro.local>
2023-03-06 15:39:13 +0000

    fix: trying to fix selenium tests

--- tests/seleniumTests/practiceUITests.js
+++ tests/seleniumTests/practiceUITests.js
@@ -4,35 +4,37 @@ const chrome = require("selenium-webdriver/chrome");
 const { Builder } = require("selenium-webdriver");
 let TIMEOUT = 30000;
 let myoptions = new chrome.Options();
-//myoptions.setChromeBinaryPath('../Drivers/chromedriver')
-myoptions.headless()
-myoptions.addArguments("disable-gpu")
-
+myoptions.headless();
+myoptions.addArguments("disable-gpu");
 
 let driver = new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(myoptions)
-    .build();
+  .forBrowser("chrome")
+  .setChromeOptions(myoptions)
+  .build();
 
-    async function example(){
+async function example() {
+  await driver
+    .manage()
+    .setTimeouts({ implicit: TIMEOUT, pageLoad: TIMEOUT, script: TIMEOUT });
+  console.info(await driver.manage().getTimeouts());
 
-      await driver.manage().setTimeouts( { implicit: TIMEOUT, pageLoad: 
-        TIMEOUT, script: TIMEOUT } );
-        console.info( await driver.manage().getTimeouts() );
+  await driver.get("http://google.com");
 
-        await driver.get("http://google.com");
-             
-        var title = await driver.getTitle();
-        assert.strictEqual(title, "Google");
-        
-        var htmlSource = await driver.getPageSource();
-        fs.appendFile("src/assets/snapshots/google-snapshot.html", htmlSource, function (err) {
-          if (err) throw err;
-          console.log("Saved!");
-        });
- 
-        //It is always a safe practice to quit the browser after execution
-        await driver.quit();
+  var title = await driver.getTitle();
+  assert.strictEqual(title, "Google");
+
+  var htmlSource = await driver.getPageSource();
+  fs.appendFile(
+    "src/assets/snapshots/google-snapshot.html",
+    htmlSource,
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
+
+  //It is always a safe practice to quit the browser after execution
+  await driver.quit();
 }
 
 example();
