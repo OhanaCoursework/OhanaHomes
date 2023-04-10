ae478e5ce407b26ff88f269ca6b0ea00e57aee9f
Declan Larkin <declanlarkin@macbook-pro.home>
2023-04-10 21:28:46 +0100

    task: adding ui tests

--- src/generalComponents/footer/Footer.js
+++ src/generalComponents/footer/Footer.js
@@ -8,48 +8,50 @@ import websiteLogo from "../../assets/images/logo/footerLogo.svg";
 
 const Footer = () => {
   return (
-    <footer className="Container">
-      <div className="Row">
-        <div className="Column1">
-          <h1>Contact Us</h1>
-          <p>
+    <footer className="Container" id="Container">
+      <div className="Row" id="Row">
+        <div className="Column1" id="Column1">
+          <h1 id="footer-heading">Contact Us</h1>
+          <p id="footer-address">
             2121 North Nimitz Highway,
             <br /> Honolulu, HI 96819,
             <br /> United States
           </p>
           <br />
-          <p>ohanahomes@gmail.com</p>
+          <p id="footer-email">ohanahomes@gmail.com</p>
           <br />
-          <p className="LastParagraph">(+1) 930-231-1231</p>
+          <p className="LastParagraph" id="footer-phone">
+            (+1) 930-231-1231
+          </p>
         </div>
         <div className="Column2 line">
-          <a href="home" className="Link">
+          <a href="home" className="Link" id="home-link">
             Home
           </a>
-          <a href="aboutUs" className="Link">
-            About
+          <a href="aboutUs" className="Link" id="about-link">
+            About Us
           </a>
-          <a href="contactUs" className="Link">
-            Contact
+          <a href="contactUs" className="Link" id="contact-link">
+            Contact Us
           </a>
-          <a href="help" className="Link">
+          <a href="help" className="Link" id="help-link">
             Help
           </a>
         </div>
         <div className="Column3 line">
-          <a href="http://www.facebook.com">
+          <a href="http://www.facebook.com" id="facebook-logo">
             <img className="icons" src={facebookIcon} />
           </a>
-          <a href="http://www.twitter.com">
+          <a href="http://www.twitter.com" id="twitter-logo">
             <img className="icons" src={twitterIcon} />
           </a>
-          <a href="http://www.instagram.com">
+          <a href="http://www.instagram.com" id="instagram-logo">
             <img className="icons" src={instagramIcon} />
           </a>
         </div>
         <div className="Column4">
           <a href="/">
-            <img className="logo" src={websiteLogo} />
+            <img className="logo" id="website-logo" src={websiteLogo} />
           </a>
         </div>
       </div>
