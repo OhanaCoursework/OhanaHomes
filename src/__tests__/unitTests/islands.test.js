b0c55b77c29aeda6907f4ed016fe0e04cb458a3c
kman8887 <69461780+kman8887@users.noreply.github.com>
2023-04-17 14:56:34 +0100

    Feature/oh 32 home page islands grid (#23)
    * Have removed some broken and redundant css please check differences or ask me to provide screenshots
    
    * remove left over debug change
    
    * Slight change to make background flash less annoying
    
    * add island grid to homepage
    
    * Fix accidental uninteded changes
    
    * Test changing to try get circle to work, it works on local
    
    * Please work by the power of black magic
    
    * Another test change
    
    * revert changes to test
    
    * change
    
    * Debug code
    
    * Ok
    
    * Fix test
    
    * Move tests to home page

---
+++ src/__tests__/unitTests/islands.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Islands from "../../Pages/home/components/Islands";
+
+configure({ adapter: new Adapter() });
+
+describe("Islands component", () => {
+  const islands = [
+    {
+      island: "Hawai'i",
+      image: "hawaiiImage.png",
+      alt: "An image of the coast of Hawai'i",
+    },
+    {
+      island: "Maui",
+      image: "mauiImage.png",
+      alt: "An image of a road on the coast of Maui",
+    },
+  ];
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Islands islandsData={islands} />);
+  });
+
+  it("renders island cards", () => {
+    expect(wrapper.find(".islandCard")).toHaveLength(islands.length);
+  });
+
+  it("renders current cards with given data", () => {
+    for (let i = 0; i < islands.length; i++) {
+      expect(wrapper.find(".islandName").at(i).text()).toEqual(
+        islands[i].island
+      );
+      expect(wrapper.find(".islandImage").at(i).prop("src")).toEqual(
+        islands[i].image
+      );
+      expect(wrapper.find(".islandImage").at(i).prop("alt")).toEqual(
+        islands[i].alt
+      );
+      expect(wrapper.find(".islandLink").at(i).prop("href")).toEqual(
+        "/islands"
+      );
+    }
+  });
+});
---
+++ src/helpers/islandsData/islandsData.js
@@ -0,0 +1,51 @@
+import hawaiiImage from "../../assets/images/islandImages/hawaii.png";
+import kahoolaweImage from "../../assets/images/islandImages/kaho'olawe.png";
+import kauaiImage from "../../assets/images/islandImages/kauai.png";
+import lanaiImage from "../../assets/images/islandImages/lanai.png";
+import mauiImage from "../../assets/images/islandImages/maui.png";
+import molokaiImage from "../../assets/images/islandImages/molokai.png";
+import niihauImage from "../../assets/images/islandImages/niihau.png";
+import oahuImage from "../../assets/images/islandImages/oahu.png";
+
+export const IslandsData = [
+  {
+    island: "Hawai'i",
+    image: hawaiiImage,
+    alt: "An image of the coast of Hawai'i",
+  },
+  {
+    island: "Maui",
+    image: mauiImage,
+    alt: "An image of a road on the coast of Maui",
+  },
+  {
+    island: "O'ahu",
+    image: oahuImage,
+    alt: "An image of a beach and buildings on O'ahu",
+  },
+  {
+    island: "Kaua'i",
+    image: kauaiImage,
+    alt: "An image of the a forested cliff and sunset on Kaua'i",
+  },
+  {
+    island: "Moloka'i",
+    image: molokaiImage,
+    alt: "An image of a sunny valley on Moloka",
+  },
+  {
+    island: "Lāna'i",
+    image: lanaiImage,
+    alt: "An image of a beach on Lāna'i",
+  },
+  {
+    island: "Ni'ihau",
+    image: niihauImage,
+    alt: "An image of the coast of Ni'ihau",
+  },
+  {
+    island: "Kaho'olawe",
+    image: kahoolaweImage,
+    alt: "An image of a lake on Kaho'olawe",
+  },
+];
