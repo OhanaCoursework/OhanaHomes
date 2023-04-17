b8a8f00615643aee729e1f307b173b7ff3bb1787
Gillon Paterson <gillonpaterson@MacBook-Pro.local>
2023-04-17 15:44:43 +0100

    chore: commit to rebase

--- src/__tests__/unitTests/contactUs.test.js
+++ src/__tests__/unitTests/contactUs.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import ContactUs from "../../Pages/contactUs/components/contactUs";
-import titleBlockImg from "../../assets/images/ContactUsTitleImage.png"
 
 configure({ adapter: new Adapter() });
 let wrapper;
