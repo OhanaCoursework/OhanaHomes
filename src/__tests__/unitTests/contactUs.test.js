7bd192c7b97ad92cfe284592e41bd3408439580e
Gillon Paterson <gillonpaterson@MacBook-Pro.broadband>
2023-04-18 22:11:49 +0100

    feature: commiting test file

--- src/__tests__/unitTests/contactUs.test.js
+++ src/__tests__/unitTests/contactUs.test.js
@@ -16,18 +16,18 @@ describe("Should Render All Components on About us Page", () => {
   });
 
   it("renders the header text", () => {
-    expect(wrapper.find("h1", {name: "PageHeading"}).text()).toEqual("Contact Us")
-    expect(wrapper.find("h2", {name: "usefulLinksHeader"}).at(0).text()).toEqual("Useful Links")
-    expect(wrapper.find("h2", {name: "getInContactHeader"}).at(1).text()).toEqual("Get in Contact")
-    expect(wrapper.find("h2", {name: "whereToFindUsHeader"}).at(2).text()).toEqual("Where to Find Us")
-    expect(wrapper.find("h3").text()).toEqual("Leave us a review or get in contact with our team")
+    expect(wrapper.find("h1", {name: "PageHeading"}).text()).toEqual("Contact Us");
+    expect(wrapper.find("h2", {name: "usefulLinksHeader"}).at(0).text()).toEqual("Useful Links");
+    expect(wrapper.find("h2", {name: "getInContactHeader"}).at(1).text()).toEqual("Get in Contact");
+    expect(wrapper.find("h2", {name: "whereToFindUsHeader"}).at(2).text()).toEqual("Where to Find Us");
+    expect(wrapper.find("h3").text()).toEqual("Leave us a review or get in contact with our team");
   });
 
   it("renders the buttons with correct text", () => {
-    expect(wrapper.find("button", {name: "LinkToAboutUs"}).at(0).text()).toEqual('About Us')
-    expect(wrapper.find("button", {name: "LinkToMarketPlace"}).at(1).text()).toEqual('Marketplace')
-    expect(wrapper.find("button", {name: "LinkToHome"}).at(2).text()).toEqual('Home')
-    expect(wrapper.find("button", {name: "sendButton"}).at(3).text()).toEqual('Send')
+    expect(wrapper.find("button", {name: "LinkToAboutUs"}).at(0).text()).toEqual('About Us');
+    expect(wrapper.find("button", {name: "LinkToMarketPlace"}).at(1).text()).toEqual('Marketplace');
+    expect(wrapper.find("button", {name: "LinkToHome"}).at(2).text()).toEqual('Home');
+    expect(wrapper.find("button", {name: "sendButton"}).at(3).text()).toEqual('Send');
   });
 
   it("renders the Iframe", () => {
