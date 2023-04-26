e1fd2ed93f4c7c15cb5d23954e91a13660156c4a
Gillon Paterson <gillonpaterson@MacBook-Pro.broadband>
2023-04-26 21:03:15 +0100

    Feature: fixing unit tests

--- src/__tests__/unitTests/featureProperties.test.js
+++ src/__tests__/unitTests/featureProperties.test.js
@@ -3,7 +3,7 @@ import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import FeaturedProperties from "../../Pages/home/components/featuredProperties";
 import HouseItem from "../../Pages/home/components/houseItem.js";
-import { CardData } from "../../helpers/featuredPropetiesData/propertiesData";
+import { CardData } from "../../helpers/featuredPropetiesData/buyPropertiesData";
 
 configure({ adapter: new Adapter() });
 
