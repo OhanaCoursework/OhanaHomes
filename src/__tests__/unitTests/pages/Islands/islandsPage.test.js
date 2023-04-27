f71deca2bec62c7376f821e1262989455591ff24
Kman <gt8887@hotmail.co.uk>
2023-04-27 22:20:09 +0100

    Fix unit tests

--- src/__tests__/unitTests/pages/Islands/islandsPage.test.js
+++ src/__tests__/unitTests/pages/Islands/islandsPage.test.js
@@ -2,12 +2,13 @@ import React from "react";
 import { shallow, configure } from "enzyme";
 import IslandsPage from "../../../../Pages/islands/components/IslandsPage";
 import Adapter from "enzyme-adapter-react-16";
+import { Island } from "../../../../helpers/islandsData/islandsEnum";
 
 configure({ adapter: new Adapter() });
 
 describe("IslandsPage component", () => {
   const islandData = {
-    island: "Hawaii",
+    island: Island.Hawaii,
     image: "hawaii.jpg",
     alt: "Hawaii",
     description: "A beautiful tropical paradise",
@@ -20,20 +21,20 @@ describe("IslandsPage component", () => {
   });
 
   it("renders the island name and image", () => {
-    expect(wrapper.find(".PageHeading").text()).toEqual("Hawaii");
+    expect(wrapper.find(".PageHeading").text()).toEqual("Hawai'i");
     expect(wrapper.find(".HeadingImage").prop("src")).toEqual("hawaii.jpg");
     expect(wrapper.find(".HeadingImage").prop("alt")).toEqual("Hawaii");
   });
 
   it("renders the description and a link to marketplace", () => {
     expect(wrapper.find("#OurStoryHeading").text()).toEqual(
-      "Why visit Hawaii?"
+      "Why visit Hawai'i?"
     );
     expect(wrapper.find(".paragraph1").text()).toEqual(
       "A beautiful tropical paradise"
     );
     expect(wrapper.find(".islandPageButton").prop("href")).toEqual(
-      "/marketplace"
+      "/marketplace?type=buy&island=hawai'i"
     );
     expect(wrapper.find(".islandPageButton").text()).toEqual(
       "View Homes On This Island"
